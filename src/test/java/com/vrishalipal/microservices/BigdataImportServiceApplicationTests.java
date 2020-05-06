2414eaa2abfa12da81c022c57e6a74b3d2170c9b
vjpal3 <vrishalipal@yahoo.com>
2020-05-05 21:50:45 -0400

    Initial commit

---
+++ src/test/java/com/vrishalipal/microservices/BigdataImportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrishalipal.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigdataImportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
