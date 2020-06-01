7063bac05eafb0c488f3196352a8720f1e97ae61
vjpal3 <vrishalipal@yahoo.com>
2020-06-01 19:21:25 -0400

    Remove unused imports

--- src/main/java/com/vrishalipal/microservices/BigdataImportServiceApplication.java
+++ src/main/java/com/vrishalipal/microservices/BigdataImportServiceApplication.java
@@ -2,7 +2,6 @@ package com.vrishalipal.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import  org.springframework.cloud.openfeign.EnableFeignClients;
 
--- src/main/java/com/vrishalipal/microservices/services/ImportServiceProxy.java
+++ src/main/java/com/vrishalipal/microservices/services/ImportServiceProxy.java
@@ -1,9 +1,7 @@
 package com.vrishalipal.microservices.services;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name="bigdata-reports-service")
 public interface ImportServiceProxy {
